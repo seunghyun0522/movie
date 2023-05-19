69e9f0553afb30b724515dec54dfa8a8087ffd74
seunghyun0522 <cvtcvt007@naver.com>
2023-05-19 23:13:42 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import styles from "./App.modules.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
