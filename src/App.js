131a291e3bdc7f8e0c1c7507b506763003c64ba3
seunghyun0522 <cvtcvt007@naver.com>
2023-05-19 23:09:39 +0900

    #6.0 introduction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Button from "./Button";
 import styles from "./App.modules.css";
+import { useState } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>hello</h1>
-      <Button text="seunghyun" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
     </div>
   );
 }
