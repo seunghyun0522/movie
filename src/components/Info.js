1bce785acf6f1750c5b741be549dd9453850d162
seunghyun0522 <cvtcvt007@naver.com>
2023-05-22 04:04:19 +0900

    change background

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,20 @@
 import proTypes from "prop-types";
 import styles from "./Movie.module.css";
-function Info({ id, name, thumbnail }) {
+function Info({ id, name, thumbnail, description }) {
   return (
     <div className={styles.info__main}>
       <h1 className={styles.info__name}>{name}</h1>
       <img src={thumbnail} className={styles.info__img} />
+      <p className={styles.info__description}>
+        {description.length < 10 ? (
+          <p>
+            There is no data on the description. But it's a really fun movie, so
+            make sure to watch it!
+          </p>
+        ) : (
+          `${description.slice(0, 300)}...`
+        )}
+      </p>
     </div>
   );
 }
